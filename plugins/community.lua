ad9226d5f996bddcde1a97c8908629178652b038
Mark Smith <msmith4@gmail.com>
2023-07-22 05:24:54 +0000

    Removed some buggy packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,10 +7,9 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
 }
